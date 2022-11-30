5a87033dc0b86f6b5a5291460456c31d5dfa6afb
Igor Alekhnovych <igoraleh@gmail.com>
2022-11-30 21:11:41 +0000

    Moved ImagePicker to Report

--- lib/domain/report.dart
+++ lib/domain/report.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Report {
   Report({required this.image, required this.currentLocation, required this.currentDateTime});
@@ -12,6 +14,17 @@ class Report {
 
   // TODO: implement compass direction (smooth_compass or flutter_compass)
   // TODO: implement dictionary of objects returned from object detector
+
+  setImage(ImageSource source) async {
+    try {
+      XFile? image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+
+      this.image = File(image.path);
+    } on PlatformException catch (e) {
+      return Future.error('Failed to Pick Image: $e');
+    }
+  }
 
   setUserComment(String value) {
     userComment = value;
